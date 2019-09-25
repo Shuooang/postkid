c21356e7f3aa39991522d79691848c8f245bc5f7
Michal Vyskocil <michal.vyskocil@gmail.com>
2019-09-25 12:22:54 +0200

    Problem: can't build postkid
    Solution: use builder submodule, drop unused code

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.12
 
 require (
 	github.com/stretchr/testify v1.4.0
+	github.com/vyskocilm/postkid/builder v0.0.0-20190925101945-1964dfbe00b7
 	gopkg.in/yaml.v2 v2.2.2
 )
+
+replace github.com/vyskocilm/postkid/builder => ./builder
--- main.go
+++ main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/url"
-	"os"
-	"strings"
 
-    "github.com/vyskocilm/postkid/builder"
-
-	yaml "gopkg.in/yaml.v2"
+	"github.com/vyskocilm/postkid/builder"
 )
 
 var (
@@ -22,39 +16,13 @@ func init() {
 	flag.BoolVar(&verbose, "verbose", false, "be verbose")
 }
 
-
-
-func processFile(file string) {
-
-	r, err := os.Open(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	b, err := ioutil.ReadAll(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var req builder.Request
-	err = yaml.Unmarshal(b, &req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//log.Printf("req=%+v", req)
-
-	fmt.Printf("%s\n", builder.Curl(&req))
-}
-
 func processFiles(files []string) {
 	for _, file := range files {
-		processFile(file)
-        req, err := builder.ParseFile(file)
-        if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Printf(builder.Curl(&req))
+		req, err := builder.ParseFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf(builder.Curl(&req))
 	}
 }
 
